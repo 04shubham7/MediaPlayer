dfe954977db3593f81354fda5ea73e27b1474bb7
Shubham <shubham040711@gmail.com>
2024-12-25 01:12:27 +0530

    added code to play,pause and stop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mediaplayer"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mediaplayer"
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures{
+        viewBinding=true
     }
 
     buildTypes {
--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -6,17 +6,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mediaplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     lateinit var mediaPlayer: MediaPlayer
+    var totaltime:Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mediaPlayer=MediaPlayer.create(this,R.raw.tamatar)
+        mediaPlayer.setVolume(1f,1f)
+        totaltime=mediaPlayer.duration
+
+
+        binding.play.setOnClickListener {
+            mediaPlayer.start()
+        }
+        binding.pause.setOnClickListener {
+            mediaPlayer.pause()
+        }
+        binding.stop.setOnClickListener {
+            mediaPlayer.stop()
+            mediaPlayer.reset()
+            mediaPlayer.release()
+        }
+//        when user changes the time stamp of music, reflect that change
+
+
     }
 }
